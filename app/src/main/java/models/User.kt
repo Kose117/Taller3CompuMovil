c5f515be09492b93fb972d1254434c66d4cb8193
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-05-05 01:49:54 -0500

    feature: added custom annotation and availability req, res

---
+++ app/src/main/java/models/User.kt
@@ -0,0 +1,3 @@
+package models
+
+data class User(val name: String, val lastname: String, val profile_picture: String)
---
+++ app/src/main/java/models/availabilityRequest.kt
@@ -0,0 +1,3 @@
+package models
+
+data class availabilityRequest(val email: String)
