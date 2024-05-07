f7e42f348ebbc0ec62fbb9e0ac623d2c3e2d1e6d
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-05-06 21:57:23 -0500

    feature: deserialize json from ws response

--- app/src/main/java/network/WebSocketListener.kt
+++ app/src/main/java/network/WebSocketListener.kt
@@ -1,6 +1,8 @@
 package network
 
 import android.util.Log
+import com.google.gson.Gson
+import models.user.locUser
 import okhttp3.*
 import java.util.concurrent.TimeUnit
 
@@ -12,7 +14,13 @@ class EchoWebSocketListener : WebSocketListener() {
 
     override fun onMessage(webSocket: WebSocket, text: String) {
         println("Receiving : $text")
-        Log.i("WEB SOCKET RECEIVING", text)
+        val user = deserializeUser(text)
+        Log.i("WEB SOCKET RECEIVING", user.toString())
+    }
+
+    fun deserializeUser(jsonString: String): locUser {
+        val gson = Gson()
+        return gson.fromJson(jsonString, locUser::class.java)
     }
 
     override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
